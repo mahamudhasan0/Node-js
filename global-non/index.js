9e9ff7de89763bf5feda9d64ce758a0338593051
Mahamud Hasan <mahmudhasan960458@gmail.com>
2022-03-22 02:18:53 +0600

    simple api create

--- global-non/index.js
+++ global-non/index.js
@@ -3,3 +3,4 @@ const fs = require('fs');
 fs.writeFileSync("first.txt","Its an non global because it has been imported")
 console.log("->>",__dirname); //check the directory
 console.log("->>",__filename); //check file name
+console.log("hello")
