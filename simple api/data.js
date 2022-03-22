cd16818a1e0cfb09d32911b00fff5b6d05ddf6e9
Mahamud Hasan <mahmudhasan960458@gmail.com>
2022-03-22 19:14:12 +0600

    first api create

---
+++ simple api/data.js
@@ -0,0 +1,6 @@
+const data = [
+    {name:'Mahamud',email:'mahmud@gmail.com',contact:'124578'},
+    {name:'Hasan',email:'mahmud@gmail.com',contact:'5874654'},
+    {name:'Mithoon',email:'mahmud@gmail.com',contact:'8797654'}
+]
+module.exports = data;
