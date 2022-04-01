bd7438f6bc163b6c0b919abfff9384f63265eb30
Mahamud Hasan <mahmudhasan960458@gmail.com>
2022-04-02 01:33:01 +0600

    render HTML and JSON

--- Express Js basic/index.js
+++ Express Js basic/index.js
@@ -1,18 +1,30 @@
-const { prototype } = require('events');
 const express = require('express');
-const { listenerCount } = require('process');
 const app = express();
 
 app.get('',(req,res)=>{
-    res.send("This is a home page");
+    res.send(`
+    <h1> This is a home page</h1>
+    <a href="/about"> Go to about</a>
+    `);
 });
 
 app.get('/about',(req,res)=>{
-    res.send("This is a about page");
+    res.send(`
+    <input type="text" placeholder="user name" value="${req.query.name}"/>
+    <button>Click Me</button>
+    <a href="/">Go to Home Page</a>
+    `);
 });
 
 app.get('/contact',(req,res)=>{
-    res.send("This is a home page");
+    res.send(`
+      [
+          {
+              name:"sam";
+              email:"sam.test@gmail.com"
+          }
+      ]
+    `);
 });
 
 app.listen(4000);
